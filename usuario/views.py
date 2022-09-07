846240d1dd5bb15efa4af26e0fef1b043de13621
dev-ana-lopes <dev.anajulia@outlook.com.br>
2022-09-06 21:44:02 -0300

    Configuração de autenticação

--- usuario/views.py
+++ usuario/views.py
@@ -1,22 +1,29 @@
 from rest_framework import viewsets,generics
 from usuario.models import Candidato, Curriculo, Formulario
 from usuario.serializer import CandidatoSerializer, CurriculoSerializer, FormularioSerializer, ListaFormularioCandidatoSerializer
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class CandidatosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os candidatos"""
     queryset = Candidato.objects.all()
     serializer_class = CandidatoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CurriculosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os curriculos"""
     queryset = Curriculo.objects.all()
     serializer_class = CurriculoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     
 class FormulariosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os formularios"""
     queryset = Formulario.objects.all()
     serializer_class = FormularioSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ListaFormularioCandidato(generics.ListAPIView):
     """Listando os formularios de um candidato"""
@@ -25,5 +32,7 @@ class ListaFormularioCandidato(generics.ListAPIView):
         return queryset
     
     serializer_class = ListaFormularioCandidatoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     
      
